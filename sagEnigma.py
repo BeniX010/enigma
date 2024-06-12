a447f8a53518f982b6fc6961e61305b831681441
NicoAez <64535405+NicoAez@users.noreply.github.com>
2024-06-12 15:47:51 +0200

    change string representation of reflector (string rep of enigma currently wrong)

--- sagEnigma.py
+++ sagEnigma.py
@@ -264,9 +264,8 @@ class Reflector:
         sage: r = Reflector([24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 
         ....: 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19])
         sage: r
-        Permutation acting as a reflector or plugboard with permutation: [24, 
-        17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2,
-        22, 21, 9, 0, 19]
+        Reflector or Plugboard using permutation [24, 17, 20, 7, 16, 18, 11, 3, 
+        15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
 
     The reflector/plugboard needs to have a length of exactly 26::
 
@@ -347,13 +346,12 @@ class Reflector:
             sage: r = Reflector([24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 
             ....: 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19])
             sage: r
-            Permutation acting as a reflector or plugboard with permutation: 
-            [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 
-            5, 25, 2, 22, 21, 9, 0, 19]
+            Reflector or Plugboard using permutation [24, 17, 20, 7, 16, 18, 
+            11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 
+            19]
         """
 
-        msg = ("Permutation acting as a reflector or plugboard with "
-               "permutation: {1}")
+        msg = "Reflector or Plugboard using permutation {0}"
         return msg.format(self._permutation)
 
 
