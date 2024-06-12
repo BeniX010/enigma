bf667c5b447159e7338de2a5f8838d89dd6b0064
NicoAez <64535405+NicoAez@users.noreply.github.com>
2024-06-12 15:53:48 +0200

    change string representation of enigma (2)

--- sagEnigma.py
+++ sagEnigma.py
@@ -381,13 +381,18 @@ class Enigma:
         ....: 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19])
         sage: e = Enigma([a, b, c], d, None)
         sage: e
-        Enigma with rotors [[21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 
-        13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10], [1, 3, 5, 7, 9,
-        11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 
-        18, 16, 14], [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 
-        12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]], reflector [24, 17, 
-        20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 
-        22, 21, 9, 0, 19] and plugboard None
+        Enigma using [Rotor with current position 24 (Initially 5), offset of 
+        2, notch at 26 and using permutation [21, 25, 1, 17, 6, 8, 19, 24, 20, 
+        15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10], Rotor 
+        with current position 24 (Initially 24), offset of 4, notch at 22 and 
+        using permutation [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 
+        24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14], Rotor with current 
+        position 11 (Initially 11), offset of 24, notch at 5 and using 
+        permutation [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 
+        16, 6, 25, 13, 15, 24, 5, 21, 14, 4]], Reflector or Plugboard using 
+        permutation [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 
+        8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19] as a reflector and None as a 
+        plugboard
 
     .. NOTE::
 
@@ -555,13 +560,18 @@ class Enigma:
             ....: 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19])
             sage: e = Enigma([a, b, c], d, None)
             sage: e
-            Enigma with rotors [[21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 
-            13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10], [1, 3, 5, 7, 9,
-            11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 
-            18, 16, 14], [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 
-            12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]], reflector [24, 17, 
-            20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 
-            22, 21, 9, 0, 19] and plugboard None
+            Enigma using [Rotor with current position 24 (Initially 5), offset 
+            of 2, notch at 26 and using permutation [21, 25, 1, 17, 6, 8, 19, 
+            24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 
+            10], Rotor with current position 24 (Initially 24), offset of 4, 
+            notch at 22 and using permutation [1, 3, 5, 7, 9, 11, 2, 15, 17, 
+            19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14], 
+            Rotor with current position 11 (Initially 11), offset of 24, notch 
+            at 5 and using permutation [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 
+            7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]], Reflector 
+            or Plugboard using permutation [24, 17, 20, 7, 16, 18, 11, 3, 15, 
+            23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19] as a 
+            reflector and None as a plugboard
         """
 
         msg = "Enigma using {0}, {1} as a reflector and {2} as a plugboard"
