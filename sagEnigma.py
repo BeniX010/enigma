24956f10bec88d35e05aaf13ceade49de643f574
NicoAez <64535405+NicoAez@users.noreply.github.com>
2024-06-12 16:27:40 +0200

    add exmples to static create method in enigm

--- sagEnigma.py
+++ sagEnigma.py
@@ -473,6 +473,46 @@ class Enigma:
         OUTPUT: Enigma with defined settings
 
         EXAMPLES::
+
+            sage: e = Enigma.create([5, 3, 2], [5, 24, 11], [2, 4, 24], 2, None)
+            sage: e
+            Enigma using [Rotor with current position 17 (Initially 5), offset 
+            of 2, notch at 26 and using permutation [21, 25, 1, 17, 6, 8, 19, 
+            24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 
+            10], Rotor with current position 24 (Initially 24), offset of 4, 
+            notch at 22 and using permutation [1, 3, 5, 7, 9, 11, 2, 15, 17, 
+            19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14], 
+            Rotor with current position 11 (Initially 11), offset of 24, notch 
+            at 5 and using permutation [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 
+            7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]], Reflector 
+            or Plugboard using permutation [24, 17, 20, 7, 16, 18, 11, 3, 15, 
+            23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19] as a 
+            reflector and None as a plugboard
+
+        The length of the list for the initials position and the list for the
+        offsets must be the same length as the list for the rotors to be used
+        since every rotor need a initial postion and offset::
+
+            sage: e = Enigma.create([5, 3, 2, 4], [5, 24], [2, 4, 24], 2, None)
+            Traceback (most recent call last):
+            ...
+            ValueError: initial_positions and rotors must be the same length
+            sage: e = Enigma.create([5, 3, 2, 4], [5, 24, 1], [2, 4], 2, None)
+            Traceback (most recent call last):
+            ...
+            ValueError: offsets and rotors must be the same length
+
+        There are not unlimited predefined rotors and reflectors::
+
+            sage: e = Enigma.create([2, 6], [2, 5], [12, 13], 2, None)
+            Traceback (most recent call last);
+            ...
+            ValueError: Values in rotors must be a integer between 1 and 5
+            sage: e = Enigma.create([2, 5], [2, 5], [12, 13], 4, None)
+            Traceback (most recent call last);
+            ...
+            ValueError: Reflector must be a value between 1 and 3
+
         """
         wheels = [[4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 
                   23, 20, 18, 15, 0, 8, 1, 17, 2, 9],
